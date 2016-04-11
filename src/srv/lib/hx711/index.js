141f2e98e6f4c29ebeeeff1d01c0529871e5f4b3
Olof Karlsson <ollekarlsson.88@gmail.com>
2016-04-11 15:59:20 +0200

    start guess for stereo values

--- src/srv/lib/hx711/index.js
+++ src/srv/lib/hx711/index.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var scale = new hx711.HX711();
 console.log(scale);
-console.log(scale.getValue);
+console.log(scale.getValues);
 
 buffer = "";
 timer = setInterval(function(){
-	buffer += scale.getValue() + ",";
+	buffer += scale.getValues() + "\n";
 }, 200);
 
 setTimeout(function(){
