1770f6acf6e19678af222135052b9187da4cdda8
Linus Berggren <linus.schwalbe@gmail.com>
2016-04-11 17:20:14 +0200

    Working.

--- src/srv/lib/hx711/index.js
+++ src/srv/lib/hx711/index.js
@@ -1,18 +1,1 @@
 module.exports = new (require('./build/Release/addon').HX711)();
-
-/*var fs = require('fs');
-
-var scale = new hx711.HX711();
-
-console.log(scale);
-console.log(scale.getValues);
-
-buffer = "";
-timer = setInterval(function(){
-	buffer += scale.getValues() + "\n";
-}, 200);
-
-setTimeout(function(){
-	clearInterval(timer);
-	fs.writeFileSync('output.csv', buffer);
-}, 10000);*/
--- src/srv/lib/system.js
+++ src/srv/lib/system.js
@@ -1,8 +1,19 @@
 module.exports = (function(object){
-  const poll = require(`${__dirname}/poll`);
+  //const poll = require(`${__dirname}/poll`);
+  const hx711 = require("hx711");
 
   object.init = function(){
-    poll();
+    buffer = "";
+    timer = setInterval(function(){
+    	buffer += hx711.getValues() + "\n";
+    }, 200);
+
+    setTimeout(function(){
+    	clearInterval(timer);
+    	console.log(buffer);
+    }, 10000);
+
+    //poll();
     //state.init();
   };
 
